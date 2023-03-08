75cdff40eb94c927751a83c7119b278cf6d4f8a1
Amanda Fernandes <124813517+amandagpf@users.noreply.github.com>
2023-03-08 13:44:25 -0300

    Commit teste

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('dark')
+    html.classList.toggle('light')
     
     const img = document.querySelector("#profile img")
     if(html.classList.contains('dark')){
-        img.setAttribute('src', '../RocketSeat/imagens/Profiledark.png')
+        img.setAttribute('src', './imagens/Profiledark.png')
     } else{
-        img.setAttribute('src', '../RocketSeat/imagens/Profile.png')
+        img.setAttribute('src', './imagens/Profile.png')
    } 
     
 }
